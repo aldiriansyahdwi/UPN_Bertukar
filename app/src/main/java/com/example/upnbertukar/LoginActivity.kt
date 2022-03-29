2d46eb2583daa7351e4464f7bdb31ed334ddc16c
Raffiza <azkaraffi66@gmail.com>
2022-03-29 23:34:10 +0700

    login page simple

--- app/src/main/java/com/example/upnbertukar/LoginActivity.kt
+++ app/src/main/java/com/example/upnbertukar/LoginActivity.kt
@@ -1,18 +1,34 @@
 package com.example.upnbertukar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.upnbertukar.databinding.ActivityLoginBinding
+import com.example.upnbertukar.placeholder.DummyProfile
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private val userId = DummyProfile.getUserID()
+
+    private val userPassword = DummyProfile.getUserPassword()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide()
+
+        with(binding){
+            btnLogin.setOnClickListener {
+                if (txtId.text.toString() == userId && txtPass.text.toString() == userPassword) {
+                    val intent = Intent(this@LoginActivity,MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/upnbertukar/placeholder/DummyProfile.kt
@@ -0,0 +1,15 @@
+package com.example.upnbertukar.placeholder
+
+object DummyProfile {
+
+    var id = "1910511046"
+    var password = "password123"
+
+    fun getUserID(): String {
+        return id
+    }
+
+    fun getUserPassword(): String {
+        return password
+    }
+}
